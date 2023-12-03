29da6a315e30aa26e346f6b47be82d3c4533f9ce
Amit kumar <ramitgzp56@gmail.com>
2023-12-04 00:27:10 +0530

    cors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const app=express();
+const cors=require('cors');
 const {registerUser,loginUser}=require('./controller/user.js')
-
+app.use(cors());
 app.use(express.json());
 app.post('/register',registerUser)
 app.post('/login',loginUser)
